957404cae7c6195774ae40c614b3c22be7804c7e
Valerii-Denisov <klerk919191@gmail.com>
2023-05-03 10:19:34 +0300

    adding html form to reset password

--- test_task/urls.py
+++ test_task/urls.py
@@ -29,13 +29,25 @@ urlpatterns = [
         'password-reset/',
         a_views.PasswordResetView.as_view(template_name='reset_password.html'),
         name='password_reset'),
-    path('password-reset/done/', a_views.PasswordResetDoneView.as_view(),
-         name='password_reset_done'
-         ),
-    path('reset/<uidb64>/<token>/', a_views.PasswordResetConfirmView.as_view(),
-         name='password_reset_confirm'
-         ),
-    path('reset/done/', a_views.PasswordResetCompleteView.as_view(),
-         name='password_reset_complete'
-         ),
+    path(
+        'password-reset/done/',
+        a_views.PasswordResetDoneView.as_view(
+            template_name='reset_password_done.html',
+        ),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        a_views.PasswordResetConfirmView.as_view(
+            template_name='reset_password_confirm.html',
+        ),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/',
+        a_views.PasswordResetCompleteView.as_view(
+            template_name='password_reset_complete.html',
+        ),
+        name='password_reset_complete'
+    ),
 ]
